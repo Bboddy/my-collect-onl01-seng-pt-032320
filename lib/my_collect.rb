e6276ad5841b87ff942504b21868b5252c098cee
puckish-system-8282 <bryce.boddy@gmail.com>
2020-04-05 00:40:11 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,9 @@
 def my_collect(collection)
-  
+  i = 0 
+  collectionArray = []
+  while array.length > i 
+    collectionArray << yield(array[i])
+    i = 1+i
+  end
+  return collectionArray
 end
