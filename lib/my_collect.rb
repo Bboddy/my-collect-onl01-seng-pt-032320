8928e8e58a7d96f9f5cd26c305d981eadbe563f5
puckish-system-8282 <bryce.boddy@gmail.com>
2020-04-05 00:41:10 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+  i = 0 
+  collectionArray = []
+  while collection.length > i 
+    collectionArray << yield(collection[i])
+    i = 1+i
+  end
+  return collectionArray
+end
